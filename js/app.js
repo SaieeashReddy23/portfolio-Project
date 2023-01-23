e423ed9b5cec2b9827abe659f441ba5801ab1049
Duddikunta Venkata Saieeash Reddy <saieeash@Saieeash-Air.local>
2023-01-23 23:05:56 +0530

    worked on protfolio project , most finished

--- js/app.js
+++ js/app.js
@@ -4,13 +4,13 @@ const closeBtn = document.querySelector("#close-btn");
 const sidebar = document.querySelector("#sidebar");
 const date = document.querySelector("#date");
 // add fixed class to navbar
-window.addEventListener("scroll", function () {
-  if (window.pageYOffset > 80) {
-    navbar.classList.add("navbar-fixed");
-  } else {
-    navbar.classList.remove("navbar-fixed");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.pageYOffset > 80) {
+//     navbar.classList.add("navbar-fixed");
+//   } else {
+//     navbar.classList.remove("navbar-fixed");
+//   }
+// });
 // show sidebar
 navBtn.addEventListener("click", function () {
   sidebar.classList.add("show-sidebar");
